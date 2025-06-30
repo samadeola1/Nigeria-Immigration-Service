32aee5696903cd6f98c6411ba3f3072c3940b539
endy mimi <ighobuendy@gmail.com>
2025-06-30 04:43:01 -0700

    get-started

--- client/src/sections/VisitorsStatus.jsx
+++ client/src/sections/VisitorsStatus.jsx
@@ -9,9 +9,9 @@ const VisitorsStatus = () => {
 
 
   <>
-  <main className='bg-green-800 h-[274px] overflow-hidden '>
+  <main className='bg-green-800 h-[274px] overflow-hidden lg:block hidden  '>
 
-  <section className=" overflow-hidden text-white px-6 md:px-16 py-8 md:py-20 md:w-12/15 container mx-auto flex flex-col md:flex-row items-center justify-between gap-6 md:gap-10 ">
+  <section className=" overflow-hidden text-white px-6 md:px-16 py-5 md:py-20 md:w-12/15 container mx-auto flex flex-col md:flex-row items-center justify-between gap-6 md:gap-10 ">
      
       <div className="text-center md:text-left">
         <h2 className="text-xl md:text-2xl font-semibold">
@@ -25,20 +25,21 @@ const VisitorsStatus = () => {
 
       {/* Center Text */}
       <div className="flex-1 text-center md:text-left text-sm md:text-base">
-        <p>
-          Regularize your immigration status in Nigeria 
-           </p>
-          <p>before the grace
-          period ends on 1st August 2025. </p>
-          <p>Take advantage of this chance to
-          regularize your </p>
-          <p>stay and avoid future immigration complications.</p>
+       <p>
+        Regularize your immigration status in Nigeria 
+           
+          before the grace
+          period ends on 1st August 2025
+        Take advantage of this chance to
+          regularize your
+          stay and avoid future immigration complications
+       </p>
        
       </div>
       
 
       
-      <button className="bg-green-500 hover:bg-[#00AA55] text-white font-semibold px-5 py-2 rounded-lg flex items-center gap-2 whitespace-nowrap">
+      <button className="bg-green-500 hover:bg-[#00AA55] text-white font-semibold  py-2 p-3 rounded-lg flex items-center gap- whitespace-nowrap">
         Click here now <FiArrowRight />
       </button>
       

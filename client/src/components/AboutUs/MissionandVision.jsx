6512fd62c0907ca00c7104cb26b84b9c0e584ab2
Olumide-q <ogunlanaabdulquadri@gmail.com>
2025-07-02 20:58:45 +0100

    updated

--- client/src/components/AboutUs/MissionandVision.jsx
+++ client/src/components/AboutUs/MissionandVision.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import { mission } from '../../db/misson'
+import MissionCard from '../reuseable/MissionCard'
 const MissionandVision = () => {
   return (
-    <div>MissionandVision</div>
+    <div className='w-full bg-white pb-[80px] lg:py-[80px] text-black'>
+      <div className='w-11/12 container mx-auto flex flex-col gap-6'>
+        <div className='flex flex-col items-center text-center'>
+          <h1 className='lg:text-[44px] md:text-[35px] text-[28px] font-bold px-5'>Our Mission and Vision</h1>
+          <p className='text-[14px] md:text-[20px] font-normal lg:w-[72%]'>Guided by purpose and driven by service, our mission and vision define our commitment to secure, efficient, and people-focused immigration management</p>
+        </div>
+        <div className='grid grid-cols-1 lg:grid-cols-2 lg:flex-row gap-[25px]  bg-white lg:text-left text-center'>
+        {
+        mission.map((item)=> (
+            <div key={item.id}>
+                  <MissionCard content={item} />
+            </div>
+        ))
+      }
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/AboutUs/NisLeaders.jsx
+++ client/src/components/AboutUs/NisLeaders.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NisLeaders = () => {
   return (
-    <div>NisLeaders</div>
+    <div></div>
   )
 }
 

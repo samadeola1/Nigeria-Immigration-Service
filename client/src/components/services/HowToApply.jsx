d695958c44c5804e53da56904b5562d3fbd1aa11
akinblaq <ogundelekn@gmail.com>
2025-07-07 08:39:48 +0100

    padding updated on apply now card to have same flow with other cards size

--- client/src/components/services/HowToApply.jsx
+++ client/src/components/services/HowToApply.jsx
@@ -22,7 +22,7 @@ const cards = [
 const HowToApply = () => {
   return (
     <div className="w-full bg-white py-10 px-4 md:px-0">
-      <div className="w-full md:w-11/12 xl:container mx-auto">
+      <div className="w-full md:w-11/12 container mx-auto">
         <h1 className="text-[28px] md:text-[44px] font-bold text-[#212121] text-center mb-8">
           How To Apply
         </h1>

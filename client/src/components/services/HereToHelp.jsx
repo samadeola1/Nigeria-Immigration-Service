d52be4e64708e27ce6125e4dc1aa93d344527b1b
samadeola1 <oroguns28@gmail.com>
2025-07-07 10:55:35 +0100

    fixes

--- client/src/components/services/HereToHelp.jsx
+++ client/src/components/services/HereToHelp.jsx
@@ -12,14 +12,14 @@ const HereToHelp = () => {
             className="w-full h-[429px] md:h-[429px] rounded-[24px] lg:rounded-[40px] object-cover"
           />
           <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white bg-black/40 px-4 rounded-[40px]">
-            <h2 className="text-2xl md:text-4xl font-bold drop-shadow-lg">
+            <h2 className="text-[24px] md:text-[44px] font-bold drop-shadow-lg">
               We’re Here to Help You
             </h2>
-            <p className="mt-2 max-w-xl text-base px-8 md:text-lg ">
+            <p className="mt-2 max-w-xl text-base  md:text-[24px] ">
               Reach out to us for assistance with your applications, services,
               or any inquiries
             </p>
-            <button className="mt-6 bg-[#00AA55] hover:bg-green-600 text-white px-8 py-3 rounded-lg font-semibold  transition-all duration-200">
+            <button className="text-[20px] mt-6 bg-[#00AA55] hover:bg-green-600 text-white px-8 py-3 rounded-lg font-semibold  transition-all duration-200">
               Contact Us
             </button>
           </div>
--- client/src/components/services/Hero.jsx
+++ client/src/components/services/Hero.jsx
@@ -8,16 +8,16 @@ const Hero = () => {
      <div className='hidden lg:block'>
         <div className='  '>
        <div className='flex flex-col gap-[24px] xl:w-[70%] '>
-         <h1 className='font-bold lg:text-[60px]'>Access <span className='text-[#00AA55]'>All Nigeria</span> Immigration Services, Fast, Secure, and Online</h1>
-         <p className='text-[24px] font-normal'>From passport applications to visa processing and permit renewals manage it all in one place with ease and reliability. </p>
+         <h1 className='font-bold text-white lg:text-[60px]'>Access <span className='text-[#00AA55]'>All Nigeria</span> Immigration Services, Fast, Secure, and Online</h1>
+         <p className='text-[24px] font-norma text-white'>From passport applications to visa processing and permit renewals manage it all in one place with ease and reliability. </p>
        </div>
           <div className='mt-4'>
-        <button className='bg-[#00AA55] hover:bg-green-700 transition-colors duration-200 px-[24px] py-[14px] rounded-md text-[18px] font-semibold '>Get started</button>
+        <button className='bg-[#00AA55] hover:bg-green-700 transition-colors duration-200 px-[24px] py-[14px] rounded-md text-[18px] text-white font-semibold '>Get started</button>
        </div>
        </div>
      </div>
       <div className='flex flex-col gap-3 lg:hidden text-center'>
-       <h1 className='text-[28px] font-bold md:px-16 '>Access All Nigeria Immigration Services, Fast, Secure, and Online</h1>
+       <h1 className='text-[28px] font-bold md:px-16 text-white '>Access All Nigeria Immigration Services, Fast, Secure, and Online</h1>
         <div>
             <button className='bg-[#00AA55]  hover:bg-green-700 transition-colors duration-200 px-[24px] py-[14px] rounded-md text-[18px] font-semibold text-white'>
               Get started

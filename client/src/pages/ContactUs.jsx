4ebfb5add04c5dd24af711483b17bfc710701297
samadeola1 <oroguns28@gmail.com>
2025-07-18 11:28:01 +0100

    fixes

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -7,29 +7,30 @@ import ContactFormSection from "../components/contact us/ContactFormSection";
 const ContactUs = () => {
   return (
     <>
-      {/* Hero section, typically full-width and responsive */}
-      <Hero />
+      <div className="bg-white">
+        {/* Hero section, typically full-width and responsive */}
+        <Hero />
 
-      {/* Main content wrapper for contact information and form */}
-      <div className="w-11/12 container  mx-auto bg-white  py-10 md:py-20 grid grid-cols-1 lg:grid-cols-2 gap-8 md:gap-12 items-stretch">
-        {/*
+        {/* Main content wrapper for contact information and form */}
+        <div className="w-11/12 container mx-auto bg-white  py-10 md:py-20 grid grid-cols-1 lg:grid-cols-2 gap-8 md:gap-12 items-stretch">
+          {/*
           Contact Form Section:
           - order-1: Appears first on mobile screens (default order in a single column).
           - lg:order-2: Moves to the second column (right side) on large screens.
         */}
-        <div className="order-2 lg:order-1">
-          <ContactFormSection />
+          <div className="order-2 lg:order-1">
+            <ContactFormSection />
+          </div>
+
+          <div className="">
+            {/* Contact Information component */}
+            <ContactInfoSection />
+          </div>
         </div>
 
-        <div className="">
-         
-          {/* Contact Information component */}
-          <ContactInfoSection />
-        </div>
+        {/* Map section, typically full-width and responsive */}
+        <MapSection />
       </div>
-
-      {/* Map section, typically full-width and responsive */}
-      <MapSection />
     </>
   );
 };
